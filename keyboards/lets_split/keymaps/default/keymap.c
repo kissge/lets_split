28a2bd587688ffea7d88e2e2d068c87373de7254
kissge <mail@yo.eki.do>
2018-09-11 17:32:08 +0900

    Update layout

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-  _______, WCLOSE,  TCLOSE,  PREV,    NEXT,    NEWTAB,  KC_MUTE, KC_VOLD, KC_VOLU, KC_F11,  KC_F12,  _______, \
-  RESET,   _______, _______, REOPEN,  _______, _______, _______, _______, _______, _______, _______, _______
+  _______, WCLOSE,  TCLOSE,  PREV,    NEXT,    NEWTAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, \
+  _______, _______, REOPEN,  KC_MUTE, KC_VOLD, KC_VOLU, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
